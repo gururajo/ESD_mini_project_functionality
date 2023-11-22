36280d522cd3520e82324c48a41a689df1af412f
gururajo <gururajotagerimail2@gmail.com>
2023-11-22 14:48:14 +0530

    setup

---
+++ spring_api_server/employee/src/test/java/io/clg_erp/employees/employee/EmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.clg_erp.employees.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
