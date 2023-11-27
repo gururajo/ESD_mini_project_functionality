eb5aab4b5051e1f929f83390503bdee38e77c700
gururajo <gururajotagerimail2@gmail.com>
2023-11-27 18:44:27 +0530

    recovered somehow deltes

---
+++ employee_api/src/main/java/clg_erp/employee_api/corsPolicy/CorsPolicy.java
@@ -0,0 +1,22 @@
+package clg_erp.employee_api.corsPolicy;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsPolicy {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:3000"); // Add the allowed origin(s)
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
--- employee_api/src/main/java/clg_erp/employee_api/employee/base_api.java
+++ employee_api/src/main/java/clg_erp/employee_api/employee/base_api.java
@@ -2,7 +2,12 @@ package clg_erp.employee_api.employee;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 
 @RestController
@@ -32,8 +37,24 @@ public class base_api {
     @RequestMapping(method = RequestMethod.POST, value = "/employee")
     public boolean AddEmployee(@RequestBody Employee employee) {
         return API_service.addEmployee(employee);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/profile_photo/{id}")
+    public boolean AddProfilePhoto(@RequestBody @RequestParam("file") MultipartFile profile_photo){
+
+        try {
+            String fileName = profile_photo.getOriginalFilename();
+            Path filePath = Path.of("C:\\MINE\\temp\\uploads", fileName);
+            Files.copy(profile_photo.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
 
     }
+
 
     @RequestMapping(method = RequestMethod.PUT, value = "/employee/{id}")
     public boolean UpdateEmployee(@PathVariable String id, @RequestBody Employee employee) {
